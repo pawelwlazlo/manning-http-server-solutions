5c9caa25a991ba852579b204c4405b126600e19a
Paweł Wlazło <pawel@wlazlo.dev>
2024-02-03 18:16:40 +0100

    control flow

---
+++ m3/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+  for i in 0..10 {
+    if let 0 = i {
+      println!("Hello WasmEdge!")
+    } else if let 1 = i {
+      println!("Howdy WasmEdge!")
+    } else if let 2 = i {
+      println!("Howdy WasmEdge!")
+    } else if let 3 = i {
+      println!("Hola WasmEdge!")
+    } else if let 4 = i {
+      println!("Bonjour WasmEdge!")
+    } else if let 5 = i {
+      println!("guten tag WasmEdge!")
+    } else if let 6 = i {
+      println!("WasmEdge 你好!")
+    } else if let 7 = i {
+      println!("こんにちは  WasmEdge!")
+    } else {
+      println!("Salve WasmEdge!")
+    }
+  }
+}
